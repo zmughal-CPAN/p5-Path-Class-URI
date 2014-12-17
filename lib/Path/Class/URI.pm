192c4698d1458a4ba6c698f0d8035270b172292e
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-12-16 22:37:35 -0600

    0.06

--- lib/Path/Class/URI.pm
+++ lib/Path/Class/URI.pm
@@ -2,7 +2,7 @@ package Path::Class::URI;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use URI;
 use URI::file;
--- lib/Path/Class/Unicode.pm
+++ lib/Path/Class/Unicode.pm
@@ -2,7 +2,7 @@ package Path::Class::Unicode;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Exporter::Lite;
 our @EXPORT = qw( ufile udir ufile_from_uri udir_from_uri );
